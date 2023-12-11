7b909e67ec92bd420cac7ba0ebc886bb4ae924e7
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-12 01:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
+
 function secondHighest(arr){
     let max = 0,secondmax=0;
     for(let i=0;i<arr.length;i++){
         if(max<arr[i]){
+             console.log("max "+max);
           max = arr[i];}
-             if(max>arr[i] && secondmax<arr[i]){
-              secondmax = arr[i];
-    
+            
    
   }
-  
+ 
+     for(let i=0;i<arr.length;i++){
+        if(max>arr[i] && secondmax<arr[i]){
+             console.log("secmax "+secondmax);
+          secondmax = arr[i];}
 }
   return secondmax;
 }
